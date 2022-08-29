402f61a888e98507cbe5fb34359aa42f3e4721e2
Choiyoungmi <choiyoungmi2252@gmail.com>
2022-08-29 13:22:33 +0900

    member finish

--- insight/admin.py
+++ insight/admin.py
@@ -1,19 +1,3 @@
 from django.contrib import admin
-from .models import Item
-
-from ast import Mod
-from email import message
-from operator import mod
-from django.db import models
-
-class ItemAdmin(admin.ModelAdmin):
-    pass
 
 
-class Post(models.Model):
-    message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.message
--- insight/models.py
+++ insight/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
@@ -11,6 +10,3 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
-
-class Item:
-    pass
